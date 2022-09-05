a95d307723639f8adc172ae855eb7154303559f3
Abrar Ahmad <abrar.dev99@gmail.com>
2022-09-05 15:10:00 +0500

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         \App\Models\User::factory()->create([
+             'email' => 'central@demo.com',
+             'password' => Hash::make('password')
+         ]);
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
